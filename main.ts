55d32b57332d7e546dd5c5d4e921969f31ac4528
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-22 11:25:03 -0400

    added the butons and genarete   the numbers

--- main.ts
+++ main.ts
@@ -5,3 +5,26 @@
  * This program generates ramdom number from 0-99
 */
 
+// seting the screen 
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// seting my varibles 
+let num1 = randint (0, 99)
+let num2 = randint (0,99)
+
+// Setting the button A and making the that to genreate a number between 0 and 99
+input.onButtonPressed(Button.A,function(){
+    basic.clearScreen()
+    basic.showString("#1:"),+ basic.showNumber(num1)
+    basic.showIcon(IconNames.Happy)
+})
+    // Setting the button A and making the that to genreate a number between 0 and 99
+input.onButtonPressed(Button.B, function(){
+    basic.clearScreen()
+    basic.showString("#2:"), + basic.showNumber(num2)
+    basic.showIcon(IconNames.Happy)
+})
+
+
+
