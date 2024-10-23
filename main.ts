11a54d8f7bfea2bb160742cb3004e80175e5a591
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-23 08:21:11 -0400

    finished the project

--- main.ts
+++ main.ts
@@ -2,46 +2,43 @@
  *
  * Created by: Emre Guzel
  * Created on: Oct 22 2024
- * This program generates ramdom number from 0-99
+ * This program generates random number from 
 */
 
-// seting the screen 
+// setting the screen 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// seting my varibles 
-let num1 = randint (0, 99)
-let num2 = randint (0, 99)
+// setting my variables 
+let num1 = randint(0, 99)
+let num2 = randint(0, 99)
 
 // Setting the button A and making the that to genreate a number between 0 and 99
-input.onButtonPressed(Button.A,function(){
+input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    basic.showString("#1:"),+ basic.showNumber(num1)
+    basic.showString("#1:"), + basic.showNumber(num1)
     basic.showIcon(IconNames.Happy)
 })
-    // Setting the button A and making the that to genreate a number between 0 and 99
-input.onButtonPressed(Button.B, function(){
+// Setting the button A and making the that to genreate a number between 0 and 99
+input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     basic.showString("#2:"), + basic.showNumber(num2)
     basic.showIcon(IconNames.Happy)
 })
 // Ading a shake functoin 
-input.onGesture(Gesture.Shake,function(){
+input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
-    if(num1 < num2){
+    if (num1 < num2) {
         basic.clearScreen()
-        basic.showNumber(num1)  
-        basic.showString("<")  
-        basic.showNumber(num2 )
+        basic.showNumber(num1)
+        basic.showString("<")
+        basic.showNumber(num2)
     }
     else {
         basic.clearScreen()
         basic.showNumber(num1)
         basic.showString(">")
         basic.showNumber(num2)
-    } 
+    }
     basic.showIcon(IconNames.Sad)
 })
-
-
- 
